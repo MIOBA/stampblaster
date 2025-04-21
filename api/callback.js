cc20b7d82db8b77c0d3b951f578d56c4f2104bd9
MIOBA <88768758+MIOBA@users.noreply.github.com>
2025-04-21 15:20:57 +0900

    Delete api/callback.js

--- api/callback.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-  const { code } = req.query;
-  if (!code) return res.status(400).send("Missing code");
-  res.status(200).send(`✅ 認証成功！Code: ${code}`);
-}
